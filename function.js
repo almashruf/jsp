699efa9ddf5e27d26f29066a32b3617c71f2b7f1
almashruf <tonoy5300@gmail.com>
2024-05-12 16:06:24 +0600

    commited

---
+++ function.js
@@ -0,0 +1,16 @@
+/*
+function sleep(name, time){
+    console.log(name +" is sleeping from "+ time)
+}
+
+sleep('Rakib', '10 PM');
+*/
+
+function myFunction(){
+    let carName='Volvo';
+    console.log(carName);
+}
+
+myFunction();
+
+//
